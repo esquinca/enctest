affa3a7b25784ef302aef0bf311f58d00ebf80fd
Unknown <esquinca_jose@hotmail.com>
2018-02-02 07:53:15 -0500

    Cambios 2

--- app/Http/Controllers/encController.php
+++ app/Http/Controllers/encController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Session;
+
+use DB;
+
 class encController extends Controller
 {    
 	public function index()
 	{
-		return view('enc1');
+		return view('main_load');
 	}
 
 	public function index2()
@@ -20,4 +24,48 @@ class encController extends Controller
 	{
 		return view('enc3');
 	}
+
+	public function index4()
+	{
+		return view('enc4');
+	}
+
+	public function cacheValues(Request $request)
+	{
+		session(['sip' => $request->sip]);
+		session(['mac' => $request->mac]);
+		session(['client_mac' => $request->client_mac]);
+		session(['uip' => $request->uip]);
+		session(['ssid' => $request->ssid]);
+		session(['vlan' => $request->vlan]);
+		session(['res' => $request->res]);
+		session(['auth' => $request->auth]);
+		return 'OK';
+	}
+
+	public function macquery()
+	{
+
+
+		//$res = DB::table('guests')->select('guest_mac')->where('guest_mac', '=', 'EC:9B:F3:6F:F6:49')->first();
+
+		$res = DB::table('guests')
+        ->whereExists(function ($query) {
+            $query->select(DB::raw(1))
+				->from('guests')
+				->where('guest_mac', '=', 'EC:9B:F3:6F:F6:47');
+            })
+            ->get();
+
+		// if ($res === TRUE) {
+		// 	//dd($res);
+		// 	return '1';
+		// }else{
+		// 	//dd($res);
+		// 	return '0';
+		// }
+
+		dd($res);
+	}
+
 }
---
+++ resources/views/main_load.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+			<a href="#" id="btnredir" onClick="btnredirpush();" style="display: none"><table cellspacing="0" cellpadding="0" width="100%"><tr><td style="background-color:#3bcdc3;border-radius:0px;color:#ffffff;display:inline-block;font-family:'Lato', Helvetica, Arial, sans-serif;font-weight:bold;font-size:13px;line-height:33px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;"><span style="color:#ffffff">Click Continuar!</span></td></tr></table></a>
+
+<!-- 			<form method="POST" action="http://{{ isset($_GET['sip']) ? $_GET['sip'] : '' }}:9997/login" id="formpr" role="login">
+				{{ csrf_field() }}
+				<input type="hidden" id="username" name="username" value="TESTX" class="form-control" />
+				<input type="hidden" id="password" name="password" value="123" class="form-control" />
+				<input class="form-control" type="hidden" id="sip" name="sip" value="{{ isset($_GET['sip']) ? $_GET['sip'] : '' }}" />
+				<input class="form-control" type="hidden" id="mac" name="mac" value="{{ isset($_GET['mac']) ? $_GET['mac'] : '' }}" />
+				<input class="form-control" type="hidden" id="client_mac" name="client_mac" value="{{ isset($_GET['client_mac']) ? $_GET['client_mac'] : '' }}" />
+				<input class="form-control" type="hidden" id="uip" name="uip" value="{{ isset($_GET['uip']) ? $_GET['uip'] : '' }}" />
+				<input class="form-control" type="hidden" id="ssid" name="ssid" value="{{ isset($_GET['ssid']) ? $_GET['ssid'] : '' }}" />
+				<input class="form-control" type="hidden" id="vlan" name="vlan" value="{{ isset($_GET['vlan']) ? $_GET['vlan'] : '' }}" />
+				<input class="form-control" type="hidden" id="res" name="res" value="{{ isset($_GET['res']) ? $_GET['res'] : '' }}" />
+				<input class="form-control" type="hidden" id="auth" name="auth" value="{{ isset($_GET['auth']) ? $_GET['auth'] : '' }}">
+			</form> -->
+			<form method="POST" action="http://172.200.5.55:9997/login" id="formpr" role="login">
+				{{ csrf_field() }}
+				<input type="hidden" id="username" name="username" value="TESTX" class="form-control" />
+				<input type="hidden" id="password" name="password" value="123" class="form-control" />
+				<input class="form-control" type="hidden" id="sip" name="sip" value="algo" />
+				<input class="form-control" type="hidden" id="mac" name="mac" value="algo" />
+				<input class="form-control" type="hidden" id="client_mac" name="client_mac" value="algo" />
+				<input class="form-control" type="hidden" id="uip" name="uip" value="algo" />
+				<input class="form-control" type="hidden" id="ssid" name="ssid" value="algo" />
+				<input class="form-control" type="hidden" id="vlan" name="vlan" value="algo" />
+				<input class="form-control" type="hidden" id="res" name="res" value="algo" />
+				<input class="form-control" type="hidden" id="auth" name="auth" value="algo">
+			</form>
+	<script src="{{ asset('/js/libs.js') }}"></script>
+	<script type="text/javascript">
+		let objData;
+		$(function() {
+			objData = $("#formpr").find("select,textarea, input").serialize();
+			init();
+		});
+
+		function init() {
+			CacheOnLoad();
+		}
+
+		function existence() {
+			$.ajax({
+			   url: "/mac_check",
+			   type: "POST",
+			   data: objData,
+			   success: function (data) {
+			     if (data === "OK") {
+			     	console.log('Cacheados.');
+			     	//$('#formpr').submit();
+			     }else{
+			     	console.log('algun error cache');
+			     	//$('#formpr').submit();
+			     }
+			   },
+			   error: function (data) {
+			     console.log('Error:', data);
+			   }
+			});
+		}
+
+		function CacheOnLoad() {
+
+			$.ajax({
+			   url: "/cache",
+			   type: "POST",
+			   data: objData,
+			   success: function (data) {
+			     if (data === "OK") {
+			     	console.log('Cacheados.');
+			     	//$('#formpr').submit();
+			     }else{
+			     	console.log('algun error cache');
+			     	//$('#formpr').submit();
+			     }
+			   },
+			   error: function (data) {
+			     console.log('Error:', data);
+			   }
+			});
+		}
+
+		function btnredirpush() {
+			objData = $("#formpr").find("select,textarea, input").serialize();
+			console.log(objData);
+			$.ajax({
+			   url: "/submit_info",
+			   type: "POST",
+			   data: objData,
+			   success: function (data) {
+			     if (data === "OK") {
+			     	console.log('entre al ok');
+			     	$('#formpr').submit();
+			     }else{
+			     	//console.log(data + 'algun error');
+			     	console.log('algun error');
+			     	$('#formpr').submit();
+			     }
+			   },
+			   error: function (data) {
+			     console.log('Error:', data);
+			   }
+			});
+			//$('#formpr').submit();
+		}
+	</script>
+
+</body>
+</html>
